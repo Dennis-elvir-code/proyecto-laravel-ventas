c684ef4ef6256227a839ce1f7f4742cf0a589efb
Dennis Elvir <dennis.elvir.code@gmail.com>
2024-02-25 12:53:35 -0600

    agregue la ruta home

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', function () {
+    return 'Bienvenidos a la pagina de inicio con ruta';
+});
